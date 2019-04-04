d9d7aed9f34b6d87db1b49c45b22e1d4e7681721
KugelbergDK <kugelberg.dk@gmail.com>
2019-04-04 19:28:17 +0200

    Init commit for AES

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("This is going to be a cool encrypting software in java");
+	    // write your code here
+
+        String plainText = "This is a secret message!";
+
+        AES aes = new AES("MyRe4llyStr0ngPassw@rd!");
+        String encrypted = aes.encrypt(plainText);
+        System.out.println("Encrypted:\t" + encrypted);
+
+        // TIme to decrypt that fucker
+        String decrypted = aes.decrypt(encrypted);
+        System.out.println("Decrypted:\t" + decrypted);
+
+
+
+
+
     }
 }
